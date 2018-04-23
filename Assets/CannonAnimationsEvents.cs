b6d57485b094a59b42ec5e3a99d42449223d9393
myrilandel <jonathan.navarro.amu@gmail.com>
2018-04-23 21:30:00 +0200

    Animations on the little pirate !

---
+++ Assets/CannonAnimationsEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonAnimationsEvents : MonoBehaviour {
+
+	[SerializeField] ParticleSystem PS_CannonShoot;
+	public void StartShooting()
+	{
+		Animator playerAnim = GetComponentInChildren<PirateAnimationsEvents>().GetComponent<Animator>();
+		PS_CannonShoot.Play();
+		playerAnim.SetTrigger("StartShooting");
+	}
+}
---
+++ Assets/PirateAnimationsEvents.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class PirateAnimationsEvents : MonoBehaviour {
+
+	[SerializeField] private GameObject cannonBallAnim;
+	[SerializeField] private GameObject cannonBallSlot;
+	private Transform cannonBallSlotParent;	
+	
+	public void StartReloadingCannon()
+	{
+		Animator cannonAnimator = transform.parent.GetComponent<Animator>();
+		cannonAnimator.SetTrigger("StartReloading");
+	}
+	
+	public void EndReloadingCannon()
+	{
+		Animator cannonAnimator = transform.parent.GetComponent<Animator>();
+		cannonAnimator.SetTrigger("EndReloading");
+	}
+	
+	public void PickUpNewCannonBall()
+	{
+		Instantiate(cannonBallAnim, cannonBallSlot.transform);
+	}
+	
+	public void DropCannonBall()
+	{
+		cannonBallSlotParent = cannonBallSlot.transform.parent;
+		cannonBallSlot.transform.SetParent(null);
+	}
+	
+	public void PickUpCannonBall()
+	{
+		cannonBallSlot.transform.SetParent(cannonBallSlotParent);
+	}
+	
+	public void DestroyHeldObject()
+	{
+		Destroy(cannonBallSlot.transform.GetChild(0).gameObject);
+	}
+}
