7c3420542b0596d9b3374939dcd66dfa4715489a
myrilandel <jonathan.navarro.amu@gmail.com>
2018-05-08 20:49:43 +0200

    PlayButton

---
+++ Assets/GUIPlayButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VelandelPiracyHill
+{
+    public class GUIPlayButton : MonoBehaviour {
+
+        public GameObject PlayButton;
+        public GameObject PlayButtonOverlayed;
+        public GameManager GameManager;
+        private void OnMouseOver()
+        {
+            Debug.Log("Hello world");
+            PlayButtonOverlayed.SetActive(true);
+            PlayButton.SetActive(false);
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                GameManager.JoinGame();
+            }
+        }
+
+        private void OnMouseExit()
+        {
+            PlayButton.SetActive(true);
+            PlayButtonOverlayed.SetActive(false);
+        }
+    }
+}
